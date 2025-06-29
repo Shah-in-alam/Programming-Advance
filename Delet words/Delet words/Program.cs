ba1c7306f0c0aedc89502e9337ce43b27004f313
Mohammad Shahin Alam <137727697+Shah-in-alam@users.noreply.github.com>
2025-06-29 23:02:29 +0200

    uploaded via drag n drop

---
+++ Delet words/Delet words/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*Given a sequence of n strings, 
+ * the task is to check if any two similar words come together
+ * and then destroy each other then print the number of words left in 
+ * the sequence after this pairwise destruction.
+ */
+namespace Delet_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string>v = new List<string>();
+            v.Add("ab");
+            v.Add("aa");
+            v.Add("aa");
+            v.Add("bcd");
+            v.Add("ab");
+            Console.WriteLine(removeConsecutiveSum(v)); 
+        }
+        public static int removeConsecutiveSum(List<string> v)
+        {
+            int n = v.Count;
+            // Start traversing the sequence  
+            for (int i = 0;i<n-1;)
+            {
+                // Compare the current string with  
+                // next one Erase both if equal 
+                if (v[i].Equals(v[i+1]))
+                {
+                    // Erase function delete the element and  
+                    // also shifts other element that's why  
+                    // i is not updated 
+                    v.RemoveAt(i);
+                    v.RemoveAt(i);
+
+                    // Update i, as to check from 
+                    // previous element again
+                     if(i>0)
+                    {
+                        i--;
+                    }
+                    // Reduce sequence 
+                    n = n - 2;
+
+                }
+                else
+                {
+                    // Increment i, if not equal 
+                    i++;
+                }
+
+            }
+            // return modified size
+            return v.Count;
+        }
+    }
+}
---
+++ remove brackets/remove brackets/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+/*Simplify a given algebraic string of characters, ‘+’, ‘-‘ operators and parentheses.
+ * Output the simplified string without parentheses.
+ */
+namespace remove_brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "(a-(b+c)+d)";
+            String S2 = "a-(b-c-(d+e)) -f";
+            Console.WriteLine(simplify(s1));
+            Console.WriteLine(simplify(S2));
+        }
+        // function to simplify the string
+        static String simplify(string str)
+        {
+            int len = str.Length;
+            // resultant string of max length equal
+            // to length of the string
+            char[] res= new char[len];
+            int index = 0, i=0;
+
+            // create empty stack
+            Stack<int> s = new Stack<int>();
+            s.Push(0);
+            while (i < len)
+            {
+                if(str[i] == '(' && i ==0)
+                {
+                    i++;
+                    continue;
+                }
+                if (str[i] =='+')
+                {
+                    // if top is 1 flip the operatof
+                    if(s.Peek() == 1 )
+                    {
+                        res[index ++] = '-';
+
+                    }
+                    // if top is  0 append the same operator
+                    if(s.Peek() == 0)
+                    {
+                        res[index ++] = '+';
+                    }
+                }
+                else if(str[i] =='-')
+                {
+                    if(s.Peek() == 1 )
+                    {
+                        res[index++] = '+';
+
+                    }
+                    else if(s.Peek() == 0)
+                    {
+                        res[index++] = '-';
+                    }
+                }
+                else if (str[i] =='(' && i>0)
+                {
+                    if (str[i-1] == '-')
+                    {
+                        // x is opposite to the top of stack
+                        int x = (s.Peek() == 1) ? 0 : 1;
+                        s.Push(x);
+                    }
+                    else if (str[i-1] =='+')
+                    {
+                        s.Push(s.Peek());
+                    }
+                }
+                // if closing parentheses pop the stack once 
+                else if (str[i] == ')')
+                {
+                    s.Pop();
+                }
+                // copy the character to the result
+                else
+                {
+                    res[index++] = str[i];
+
+                }
+                i++;
+            }
+            return new String(res);
+        }
+    }
+}
