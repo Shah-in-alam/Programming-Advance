29a7fd1f4639c256b200280b04d550f11c75ea7e
Mohammad Shahin Alam <137727697+Shah-in-alam@users.noreply.github.com>
2025-06-30 19:51:13 +0200

    uploaded

---
+++ data/Data Structure/Notes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure
+{
+    internal class Notes
+    {
+        public string Data { get; set; }
+        public Notes next { get; set; }
+
+        public Notes( string data)
+        {
+            Data = data;
+            next = null;
+
+        }
+      
+    }
+}
---
+++ data/Data Structure/listed.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure
+{
+    internal class listed
+    {
+        public Notes Head{  get; set; }
+        public listed()
+        {
+            Head = null;
+        }
+        public listed(string data)
+        {
+                Head= new Notes(data);
+        }
+        public void AddStart(string data)
+        {
+            if(Head == null)
+            {
+
+                Head = new Notes(data);
+
+             }
+            else
+            {
+                Notes node =new Notes(data);
+                node.next = Head;
+                Head = node;
+            }
+        }
+        private Notes Last()
+        {
+            Notes node =Head;
+            while(node != null)
+            {
+                node = node.next;
+            }
+            return node;
+        }
+        public void AddEnd(string data)
+        {
+            if(Head == null)
+            {
+                Head = new Notes(data);
+            }
+            else
+            {
+                Last().next = new Notes(data);
+            }
+        }
+        public override string ToString()
+        {
+            string linkedlist = "";
+            Notes node = Head;
+            while(node != null)
+            {
+                linkedlist += node.Data + " --> ";
+                node = node.next;
+               
+            }
+            return linkedlist;
+        }
+        public void Delete(string data)
+        {
+            if (Head.Data == data)
+            {
+               Head = Head.next;
+            }
+            else
+            {
+                Notes node = Head;
+                while(node.next != null)
+                {
+                    if(node.next.Data == data)
+                    {
+                        if (node.next != Last())
+                            node.next = node.next.next;
+                        else node.next = null;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
