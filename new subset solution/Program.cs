9e67cc776fa9e011dbd68a95f0bdeb08979b1d69
Mohammad Shahin Alam <137727697+Shah-in-alam@users.noreply.github.com>
2025-07-04 21:35:11 +0200

    direct uplosded

---
+++ new subset solution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace new_subset_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>ints = new List<int> { 1, 2, 3 };
+            Set set = new Set();
+            List<List<int>> list = set.Solve(ints);
+            foreach(var subset  in list) 
+            {
+                Console.WriteLine(string.Join(" ",subset));
+            }
+        }
+    }
+}
---
+++ new subset solution/Set.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_subset_solution
+{
+    internal class Set
+    {
+        public List<List<int>> Solve(List<int> nums)
+        {
+            List<List<int>> result = new List<List<int>>();
+            GenerateSubsets(0, nums, new List<int>(), result);
+            return result;
+        }
+
+        private void GenerateSubsets(int index, List<int> nums, List<int> current, List<List<int>> result)
+        {
+            if (index == nums.Count)
+            {
+               result.Add(new List<int>(current));
+               Console.WriteLine($"Adding subset:{string.Join(",", current)}");
+                
+                return;
+            }
+
+            
+            current.Add(nums[index]);
+            Console.WriteLine($"Including {nums[index]}: {string.Join(",", current)}");
+
+            GenerateSubsets(index + 1, nums, current, result);
+            current.RemoveAt(current.Count - 1);
+            Console.WriteLine($"Backtrack after including {nums[index]}: {string.Join(",", current)}");
+
+            GenerateSubsets(index + 1, nums, current, result);
+            Console.WriteLine($"After excluding {nums[index]}: {string.Join(",", current)}");
+        }
+    }
+}
